d33d6f64e35e0c211e147695fa9682595190080e
wangchengkai <princewck@gmail.com>
2017-05-28 00:15:15 +0800

    index.html

--- src/scripts/templates/templates.js
+++ src/scripts/templates/templates.js
@@ -1,3 +1,3 @@
-angular.module('app').run(['$templateCache', function($templateCache) {$templateCache.put('home/home.html','<div>this is home2</div>');
+angular.module('app').run(['$templateCache', function($templateCache) {$templateCache.put('home/home.html','<div class="page-home">\n    \n</div>');
 $templateCache.put('readme/readme.html','<div class="readme">\n    <h1>\u5173\u4E8E\u7EC4\u5EFA</h1>\n</div>');
 $templateCache.put('select-panel/template.html','<div>\u6307\u4EE4</div>');}]);
